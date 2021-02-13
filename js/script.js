ed444200c3cb2b64e1271e3475146d412b10de23
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-02-13 23:20:46 +0600

    Script Added

--- js/script.js
+++ js/script.js
@@ -8,10 +8,20 @@ const searchSongs = () => {
 }
 
 const displaySongs = songs => {
-    const songContainer = document.getElementById('song-ul')
+    const songContainer = document.getElementById('song-container')
     songs.forEach(song => {
-        const li = document.createElement("li");
-        li.innerText = song.title;
-        songContainer.appendChild(li)
+        const songDiv = document.createElement("div");
+        songDiv.className = "single-result row align-items-center my-3 p-3";
+        songDiv.innerHTML = `
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+        </div>
+
+        <div class="col-md-3 text-md-right text-center">
+            <button class="btn btn-success">Get Lyrics</button>
+        </div>
+        `
+        songContainer.appendChild(songDiv);
     })
 }
