5c2f8964831567316fcfa8e9ac199a91cdcb2c04
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-02-14 00:22:50 +0600

    Script

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-const searchSongs = () => {
+const searchSongs = async() => {
     const searchText = document.getElementById('search-field').value;
-    const url = `https://api.lyrics.ovh/suggest/:${searchText}`;
+    const url = `https://api.lyrics.ovh/suggest/${searchText}`;
     // load data
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySongs(data.data))
+    const res = await fetch(url)
+    const data = await res.json()
+    displaySongs(data.data)
 }
 
 const displaySongs = songs => {
     const songContainer = document.getElementById('song-container')
+    songContainer.innerHTML = " "
     songs.forEach(song => {
         const songDiv = document.createElement("div");
         songDiv.className = "single-result row align-items-center my-3 p-3";
@@ -29,6 +30,13 @@ const displaySongs = songs => {
     })
 }
 
-const getLyrics = (artist, title) => [
-    console.log(artist, title)
-]
+const getLyrics = async(artist, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+    const res = await fetch(url)
+    const data = await res.json()
+    displayLyrics(data.lyrics)
+}
+const displayLyrics = lyrics => {
+    const lyricsDiv = document.getElementById("song-lyrics")
+    lyricsDiv.innerText = lyrics
+}
