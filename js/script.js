af64f90e384740f7f2dc9b52f0bfed43cb15e1a5
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-02-13 23:41:46 +0600

    Script

--- js/script.js
+++ js/script.js
@@ -16,12 +16,19 @@ const displaySongs = songs => {
         <div class="col-md-9">
             <h3 class="lyrics-name">${song.title}</h3>
             <p class="author lead">Album by <span>${song.artist.name}</span></p>
+            <audio controls>
+                <source src="${song.preview}">
+            </audio>
         </div>
 
         <div class="col-md-3 text-md-right text-center">
-            <button class="btn btn-success">Get Lyrics</button>
+            <button onclick="getLyrics('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
         </div>
         `
         songContainer.appendChild(songDiv);
     })
 }
+
+const getLyrics = (artist, title) => [
+    console.log(artist, title)
+]
