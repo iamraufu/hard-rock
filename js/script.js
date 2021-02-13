3a1a59107ef8e6a653f51f142f4f21185c30fc59
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-02-14 02:14:25 +0600

    Script Added

--- js/script.js
+++ js/script.js
@@ -1,42 +1,74 @@
-const searchSongs = async() => {
+// const searchSongs = async () => {
+//     const searchText = document.getElementById('search-field').value;
+//     const url = `https://api.lyrics.ovh/suggest/${searchText}`
+//     // load data
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     displaySongs(data.data);
+// }
+
+const searchSongs = () => {
     const searchText = document.getElementById('search-field').value;
-    const url = `https://api.lyrics.ovh/suggest/${searchText}`;
-    // load data
-    const res = await fetch(url)
-    const data = await res.json()
-    displaySongs(data.data)
+    const url = `https://api.lyrics.ovh/suggest/${searchText}`
+        // load data
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySongs(data.data))
+        .catch(error => displayError(error));
 }
 
+const errorTag = document.getElementById('error-message');
+const lyricsDiv = document.getElementById('song-lyrics');
+
 const displaySongs = songs => {
-    const songContainer = document.getElementById('song-container')
-    songContainer.innerHTML = " "
+    const songContainer = document.getElementById('song-container');
+    songContainer.innerHTML = '';
+    errorTag.innerText = " "
+    lyricsDiv.innerText = " "
+    document.getElementById("search-field").value = " "
     songs.forEach(song => {
-        const songDiv = document.createElement("div");
-        songDiv.className = "single-result row align-items-center my-3 p-3";
+        const songDiv = document.createElement('div');
+        songDiv.className = 'single-result row align-items-center my-3 p-3';
         songDiv.innerHTML = `
         <div class="col-md-9">
             <h3 class="lyrics-name">${song.title}</h3>
             <p class="author lead">Album by <span>${song.artist.name}</span></p>
             <audio controls>
-                <source src="${song.preview}">
+                <source src="${song.preview}" type="audio/mpeg">
             </audio>
+            <img src="${song.artist.picture_medium}">
         </div>
-
         <div class="col-md-3 text-md-right text-center">
-            <button onclick="getLyrics('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
+            <button onclick="getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
         </div>
-        `
+        `;
         songContainer.appendChild(songDiv);
     })
 }
 
-const getLyrics = async(artist, title) => {
+const getLyric = async(artist, title) => {
     const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
-    const res = await fetch(url)
-    const data = await res.json()
-    displayLyrics(data.lyrics)
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayLyrics(data.lyrics);
+    } catch (error) {
+        displayError('Sorry! I failed to load lyrics, Please try again later!!!')
+    }
 }
+
+// const getLyric = (artist, title) => {
+//     const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+//     fetch(url)
+//     .then(res => res.json())
+//     .then(data => displayLyrics(data.lyrics))
+// }
+
 const displayLyrics = lyrics => {
-    const lyricsDiv = document.getElementById("song-lyrics")
-    lyricsDiv.innerText = lyrics
+    document.getElementById('song-container').innerText = " "
+    lyricsDiv.innerText = lyrics;
+}
+
+const displayError = error => {
+    errorTag.innerText = error;
 }
